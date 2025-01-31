01683b34de13f984a1a4b184cf00caa146c7b354
Leonardo Farias <leoo.farias@gmail.com>
2025-01-30 22:52:28 -0300

    feat: add log

--- src/modules/email/functions/send-email-consumer.ts
+++ src/modules/email/functions/send-email-consumer.ts
@@ -11,9 +11,13 @@ interface EmailMessage {
 const emailService = new EmailService();
 
 export const run: SQSHandler = async (event) => {
+  console.log("Event:", event);
+
   for (const record of event.Records) {
     try {
       const { subject, eventType, emails, message }: EmailMessage = JSON.parse(record.body);
+
+      console.log({ subject, eventType, emails, message });
 
       if (eventType === "SEND_EMAIL") {
         const emailPromises = emails.map(email => {
